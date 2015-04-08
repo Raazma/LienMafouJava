bf55512aba80652108547c40afd96e85146bd6d1
fredpaquette <frederic_paquette@hotmail.com>
2015-04-08 16:57:57 -0400

    pres fini reste la boucle pour ecrire les string

---
+++ JavaExctracteurdelien/Extract.java
@@ -0,0 +1,60 @@
+import  java.io.*;
+
+public class Extract
+{
+StringBuffer buffer = new StringBuffer();
+
+   public void Read(String File,String FileDestination)
+   {
+      try
+      {
+       BufferedReader reader =  reader = new BufferedReader(
+               new FileReader(File) );
+        String line;
+          
+         while((line = reader.readLine())!= null)
+             buffer.append(line);
+             
+             reader.close();
+         
+            Extraction(buffer,FileDestination);
+       }
+       catch(IOException e)
+       {
+       }
+       Extraction(buffer,FileDestination);
+         
+   }
+    
+   private void Extraction(StringBuffer fullFile , String FileDestination )
+   {
+   String line;
+   int i = 0;
+     try
+     {
+       PrintWriter  writer = new PrintWriter(
+                     new BufferedWriter(
+                        new FileWriter( FileDestination) ) );
+                        
+              while( i + 3 !=buffer.legnt() )
+              {
+              writer.println(buffer.substring(buffer.indexOf("<a"), i = buffer.indexOf("/a>") + 3)); 
+         
+                       
+              writer.close();
+             
+              }
+     }
+     catch(IOException e)
+      {
+      
+      }
+   
+   
+   }
+   public static void main(String []args)
+   {
+     Extract tracteur = new Extract();
+     tracteur.Read(args[0],args[1]);
+   }
+}
