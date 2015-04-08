2437caf31c6e87d98ce3778383a9c5b29b6eee60
fredpaquette <frederic_paquette@hotmail.com>
2015-04-08 18:23:55 -0400

    WHEW

--- JavaExctracteurdelien/Extract.java
+++ JavaExctracteurdelien/Extract.java
@@ -11,21 +11,20 @@ StringBuffer buffer = new StringBuffer();
        BufferedReader reader =  reader = new BufferedReader(
                new FileReader(File) );
         String line;
-          
+
          while((line = reader.readLine())!= null)
              buffer.append(line);
-             
+
              reader.close();
-         
             Extraction(buffer,FileDestination);
        }
        catch(IOException e)
        {
        }
        Extraction(buffer,FileDestination);
-         
+
    }
-    
+
    private void Extraction(StringBuffer fullFile , String FileDestination )
    {
    String line;
@@ -35,22 +34,24 @@ StringBuffer buffer = new StringBuffer();
        PrintWriter  writer = new PrintWriter(
                      new BufferedWriter(
                         new FileWriter( FileDestination) ) );
-                        
-              while( i + 3 !=buffer.legnt() )
+
+
+              while(i< buffer.lastIndexOf("/a>") + 3)
               {
-              writer.println(buffer.substring(buffer.indexOf("<a"), i = buffer.indexOf("/a>") + 3)); 
-         
-                       
-              writer.close();
-             
-              }
+              writer.println(buffer.substring(buffer.indexOf("<a",i), buffer.indexOf("/a>",i) + 3));            
+              i = buffer.indexOf("/a>",i) + 3;
+              System.out.println(i);
+            //  System.out.println(buffer.indexOf("/a>",));
+            }
+            writer.close();
+
      }
      catch(IOException e)
       {
-      
+
       }
-   
-   
+
+
    }
    public static void main(String []args)
    {
